b349fecb42a04e3d3fff2985b14fcbab45af91a3
Alan Cristiano <cr.alan9119@gmail.com>
2025-04-06 01:39:51 -0300

    first commit - Spring Initializr

---
+++ src/main/java/com/debugburger/DebugBurger/DebugBurgerApplication.java
@@ -0,0 +1,13 @@
+package com.debugburger.DebugBurger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugBurgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebugBurgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debugburger/DebugBurger/DebugBurgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debugburger.DebugBurger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugBurgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
